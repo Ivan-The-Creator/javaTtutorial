20a8cfa01a23e06a28069ff1271feda4490ca2be
Ivan-The-Creator <potattosoul@gmail.com>
2025-02-14 18:31:40 +0200

    learning method overriding

---
+++ src/firstProject/Animal.java
@@ -0,0 +1,9 @@
+package firstProject;
+
+public class Animal {
+	
+	void speak() {
+		System.out.println("The animal speaking");
+	}
+
+}
---
+++ src/firstProject/methodOverriding.java
@@ -0,0 +1,15 @@
+package firstProject;
+
+public class methodOverriding {
+	
+	public static void main(String[] args) {
+		
+		Dog dog = new Dog();
+		Animal animal = new Animal();
+		
+		dog.speak();
+		animal.speak();
+		
+	}
+	
+}
